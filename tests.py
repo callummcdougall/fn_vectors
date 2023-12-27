f323c6a5d388012d1c1f20bd8d290c1723b3fdc8
Callum McDougall <cal.s.mcdougall@gmail.com>
2023-12-27 21:34:54 +0000

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,6 +4,8 @@ import torch as t
 from torch import Tensor
 from pathlib import Path
 import string
+import nnsight
+from nnsight import LanguageModel
 
 N_LAYERS = 28
 N_HEADS = 16
@@ -15,12 +17,7 @@ root = Path(__file__).parent
 
 
 
-class AntonymSequence:
-    '''
-    Class to store a single antonym sequence.
-
-    Uses the default template "Q: {x}\nA: {y}" (with separate pairs split by "\n\n").
-    '''
+class ICLSequence:
     def __init__(self, word_pairs: List[List[str]]):
         self.word_pairs = word_pairs
         self.x, self.y = zip(*word_pairs)
@@ -44,49 +41,41 @@ class AntonymSequence:
         '''Prints a readable string representation of the prompt & completion (indep of template).'''
         return f"{', '.join([f'({x}, {y})' for x, y in self[:-1]])}, {self.x[-1]} ->".strip(", ")
 
-class AntonymDataset:
-    '''
-    Dataset to create antonym pair prompts, in ICL task format. We use random seeds for consistency
-    between the corrupted and clean datasets.
-
-    Inputs:
-        word_pairs: list of antonym pairs, e.g. [["old", "young"], ["top", "bottom"], ...]
-        corrupted: if True, then the second word in each pair is replaced with a random word
-        size = number of prompts to generate
-        n_prepended = number of antonym pairs before the single word
-    '''
-
+class ICLDataset:
     def __init__(
         self,
         word_pairs: List[List[str]],
         size: int,
         n_prepended: int,
+        bidirectional: bool = True,
         corrupted: bool = False,
         seed: int = 0,
     ):
         assert n_prepended+1 <= len(word_pairs), "Not enough antonym pairs in dataset to create prompt."
-        
-        self.seed = seed
-        self.size = size
-        self.n_prepended = n_prepended
-        self.corrupted = corrupted
+
         self.word_pairs = word_pairs
         self.word_list = [word for word_pair in word_pairs for word in word_pair]
+        self.size = size
+        self.n_prepended = n_prepended
+        self.bidirectional = bidirectional
+        self.corrupted = corrupted
+        self.seed = seed
 
         self.seqs = []
         self.prompts = []
         self.completions = []
 
-        # Generate the dataset (by choosing random antonym pairs, and constructing `AntonymSequence` objects)
+        # Generate the dataset (by choosing random antonym pairs, and constructing `ICLSequence` objects)
         for n in range(size):
             np.random.seed(seed + n)
             random_pairs = np.random.choice(len(self.word_pairs), n_prepended+1, replace=False)
             random_orders = np.random.choice([1, -1], n_prepended+1)
+            if not(bidirectional): random_orders[:] = 1
             word_pairs = [self.word_pairs[pair][::order] for pair, order in zip(random_pairs, random_orders)]
             if corrupted:
                 for i in range(len(word_pairs) - 1):
                     word_pairs[i][1] = np.random.choice(self.word_list)
-            seq = AntonymSequence(word_pairs)
+            seq = ICLSequence(word_pairs)
 
             self.seqs.append(seq)
             self.prompts.append(seq.prompt())
@@ -94,7 +83,7 @@ class AntonymDataset:
 
     def create_corrupted_dataset(self):
         '''Creates a corrupted version of the dataset (with same random seed).'''
-        return AntonymDataset(self.word_pairs, self.size, self.n_prepended, True, self.seed)
+        return ICLDataset(self.word_pairs, self.size, self.n_prepended, True, self.seed)
 
     def __len__(self):
         return self.size
@@ -104,37 +93,87 @@ class AntonymDataset:
 
 
 
-def test_calculate_h(calculate_h: Callable[[AntonymDataset, int], Tuple[List[str], Tensor]]) -> None:
+def test_calculate_h(calculate_h: Callable[[ICLDataset, int], Tuple[List[str], Tensor]], model: LanguageModel, solution: bool = False) -> None:
+    '''
+    Because this function doesn't use the antonyms dataset, we can run it deterministically (i.e. without re-loading the words from
+    the text file). So it compares the output to a saved vector solution.
 
-    # This same code was used to initially calculate the fn vector (everything is deterministic)
+    Run with `solution=True` to save the vector we get from this function, rather than testing it against the previously saved vector.
+    '''
+    if "GPTJModel" not in str(model):
+        print("Warning - This test may not work with models other than GPTJ-6B.")
+
+    # This same code was used to initially calculate the fn vector (so everything is deterministic)
     word_pairs = list(zip(string.ascii_lowercase, string.ascii_uppercase))
-    dataset = AntonymDataset(word_pairs, size=5, n_prepended=5)
-    model_completions, h = calculate_h(dataset, 9)
+    dataset = ICLDataset(word_pairs, size=5, n_prepended=5)
+    model_completions, h = calculate_h(model, dataset, layer=9)
 
     # Check model completions
     assert model_completions == [" L", " K", " m", " Q", " K"], "Unexpected model completions."
 
+    # Save new vector (for future tests) if solution=True
+    if solution:
+        t.save(h, root / "data" / "test_h.pt")
+        print("Saved new h-vector.")
+        return
+
     # Check h-vector against expected vector (which was saved using this same code)
     assert h.shape == (D_MODEL,), f"Expected shape (d_model,), got {h.shape}"
-    test_h = t.load(root / "data" / "test_h.pt")
-    t.testing.assert_close(h, test_h, msg="Correct shape, but incorrect values.")
+    test_h = t.load(root / "data" / "test_h.pt", map_location='cpu')
+    t.testing.assert_close(h, test_h, msg=f"Correct shape, but incorrect values: max diff = {t.max(t.abs(h - test_h))}, mean diff = {t.mean(t.abs(h - test_h))}")
 
     print("All tests in `test_calculate_h` passed.")
 
 
 
-def test_calculate_fn_vector(calculate_fn_vector: Callable[[AntonymDataset, List[Tuple[int, int]]], Tensor]) -> None:
+def test_intervene_with_h(intervene_with_h: Callable[[ICLDataset, int, int, list], Tuple[ICLDataset, List[str], List[str]]], model, h, ANTONYM_PAIRS) -> None:
+    '''
+    Unlike the previous test, this one does use the antonyms dataset, so we have to load in this data and actually run both the
+    solution and the student's code.
+    '''
+    if "GPTJModel" not in str(model):
+        print("Warning - This test may not work with models other than GPTJ-6B.")
+
+    import solutions
+
+    # Get non-deterministic datasets
+    zero_shot_dataset = ICLDataset(ANTONYM_PAIRS, size=5, n_prepended=0)
+    
+    # Run the intervene_with_h function, get the output
+    print("Running your `intervene_with_h` function...")
+    completions_zero_shot, completions_intervention = intervene_with_h(model, zero_shot_dataset, h, layer=9)
+    print("Running `solutions.intervene_with_h` (so we can compare outputs) ...")
+    completions_zero_shot_soln, completions_intervention_soln = solutions.intervene_with_h(model, zero_shot_dataset, h, layer=9)
+
+    # Check each one is the same
+    print("Comparing the outputs...")
+    assert completions_zero_shot == completions_zero_shot_soln, "The model's zero-shot completions (without intervention) are not correct."
+    assert completions_intervention == completions_intervention_soln, "The model's zero-shot completions (without intervention) are correct, but the model's zero-shot completions (when intervening with h) are not correct."
+
+    print("\nAll tests in `test_intervene_with_h` passed.")
+
+
+
+def test_calculate_fn_vector(calculate_fn_vector: Callable[[ICLDataset, List[Tuple[int, int]]], Tensor], model, solution: bool = False) -> None:
 
     # This same code was used to initially calculate the fn vector (everything is deterministic)
     word_pairs = list(zip(string.ascii_lowercase, string.ascii_uppercase))
-    dataset = AntonymDataset(word_pairs, size=3, n_prepended=2)
-    fn_vector = calculate_fn_vector(dataset, [(8, 1)])
+    dataset = ICLDataset(word_pairs, size=3, n_prepended=2)
+    fn_vector = calculate_fn_vector(model, dataset, [(8, 1)])
 
     # Check shape
     assert fn_vector.shape == (D_MODEL,), f"Expected shape (d_model,), got {fn_vector.shape}"
+
+    # Save new vector (for future tests) if solution=True
+    if solution:
+        t.save(fn_vector, root / "data" / "test_fn_vector.pt")
+        print("Saved new fn vector.")
+        return
 
     # Load in the previously calculated fn vector, and test for equality
     fn_vector_expected = t.load(root / "data" / "test_fn_vector.pt")
     t.testing.assert_close(fn_vector, fn_vector_expected, msg="Correct shape, but incorrect values.")
 
-    print("All tests in `test_calculate_fn_vector` passed.")
+    print("\nAll tests in `test_calculate_fn_vector` passed.")
+
+
